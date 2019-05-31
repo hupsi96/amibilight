4fa9f2a7e66b48895c9918f2421d724395e3f307
Christian Huppertz <christian.huppertz@tum.de>
2019-05-31 16:07:05 +0200

    test

--- strip.py
+++ strip.py
@@ -6,9 +6,9 @@ class strip_config:
 
 
     LED_COUNT      = 57      # Number of LED pixels.
-    LED_PIN        = 12      # GPIO pin connected to the pixels (must support PWM!).
+    LED_PIN        = 13     # GPIO pin connected to the pixels (must support PWM!).
     LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-    LED_DMA        = 11      # DMA channel to use for generating signal (try 10)
+    LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
     LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
     LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
     LED_CHANNEL    = 0
